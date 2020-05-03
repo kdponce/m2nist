f8c7ce90007fee431c9326d4f8d897f8a907cde3
Keith Lorenz Ponce <kdponce@up.edu.ph>
2020-05-03 14:30:31 +0800

    Modified make_dataset script to process dataset

--- README.md
+++ README.md
@@ -6,23 +6,21 @@ Directory Structure
 
     .
     ├── AUTHORS.md
-    ├── LICENSE
     ├── README.md
-    ├── models  <- compiled model .pkl or HDFS or .pb format
     ├── config  <- any configuration files
     ├── data
-    │   ├── interim <- data in intermediate processing stage
     │   ├── processed <- data after all preprocessing has been done
     │   └── raw <- original unmodified data acting as source of truth and provenance
     ├── docs  <- usage documentation or reference papers
+    ├── models  <- compiled model .pkl or HDFS or .pb format
     ├── notebooks <- jupyter notebooks for exploratory analysis and explanation 
     ├── reports <- generated project artefacts eg. visualisations or tables
     │   └── figures
     └── src
-        ├── data-proc <- scripts for processing data eg. transformations, dataset merges etc. 
-        ├── viz  <- scripts for visualisation during EDA, modelling, error analysis etc. 
+        ├── data <- scripts for processing data eg. transformations, dataset merges etc. 
+        ├── visualization  <- scripts for visualisation during EDA, modelling, error analysis etc. 
         ├── modeling    <- scripts for generating models
     |--- environment.yml <- file with libraries and library versions for recreating the analysis environment
 
 --------------------
-"[Multidigit MNIST (M2NIST) Dataset](https://www.kaggle.com/farhanhubble/multimnistm2nist)" by [farhanhubble](https://www.kaggle.com/farhanhubble) is licensed under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)
+"[Multidigit MNIST (M2NIST) Dataset](https://www.kaggle.com/farhanhubble/multimnistm2nist)" by [farhanhubble](https://www.kaggle.com/farhanhubble) is licensed under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/).
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,40 +1,80 @@
+"""Generate dataset.
+
+Download M2NIST Dataset from Kaggle and perform processing as needed.
+"""
 import zipfile
 import sys
 import os
+import numpy as np
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATASET_PATH = 'farhanhubble/multimnistm2nist'
-DIR_PATH = SCRIPT_DIR + (2 * (os.sep + os.pardir)) + '/data/raw'
-
-# Checks if API token has been setup.
+# Check if API token has been setup.
 try:
     from kaggle.api.kaggle_api_extended import KaggleApi
 except OSError as e:
     sys.exit(e)
 
+DATASET_URL = 'farhanhubble/multimnistm2nist'
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = SCRIPT_PATH + 2 * (os.sep + os.pardir) + '/data'
+RAW_DATA_PATH = os.path.join(DATA_PATH, 'raw')
+PROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'processed')
+
 api = KaggleApi()
 api.authenticate()
 
 # Download M2NIST dataset
-if not os.path.exists(os.path.join(DIR_PATH, 'multimnistm2nist.zip')):
-    print('Downloading M2NIST dataset...', end=' ')
-    api.dataset_download_files('farhanhubble/multimnistm2nist', path=DIR_PATH)
+if not os.path.exists(os.path.join(RAW_DATA_PATH, 'multimnistm2nist.zip')):
+    print('Downloading M2NIST dataset...', end='')
+    api.dataset_download_files(DATASET_URL, path=RAW_DATA_PATH)
     print('Done.')
 else:
     print('Dataset already downloaded.')
 
-# Checks if extracted files already exist. Ignore existing files.
-dataset = zipfile.ZipFile(os.path.join(DIR_PATH, 'multimnistm2nist.zip'), 'r')
+# Check if extracted files already exist. Ignore existing files.
+dataset = zipfile.ZipFile(os.path.join(RAW_DATA_PATH, 'multimnistm2nist.zip'), 'r')
 files = dataset.namelist()
 for f in files:
-    if not os.path.exists(os.path.join(DIR_PATH, f)):
-        print('Extracting {}...'.format(f), end=' ')
-        dataset.extract(f, path=DIR_PATH)
+    if not os.path.exists(os.path.join(RAW_DATA_PATH, f)):
+        print('Extracting {}...'.format(f), end='')
+        dataset.extract(f, path=RAW_DATA_PATH)
         print('Done.')
     else:
         print('{} already exists.'.format(f))
 
 for f in files:
-    path = os.path.join(DIR_PATH, f)
+    path = os.path.join(RAW_DATA_PATH, f)
+    print('Converting {} to read-only...'.format(f), end='')
     os.chmod(path, 0o444)
-    print('Converted {} to read-only.'.format(f))
+    print('Done.')
+
+# Load dataset
+print('Processing datasets...', end='')
+data = np.load(os.path.join(RAW_DATA_PATH, 'combined.npy')).astype('float16')
+labels = np.load(os.path.join(RAW_DATA_PATH, 'segmented.npy')).astype('uint8')
+
+# Rescale input from [0,255] to [0,1] and reshape to (N,H,W,C)
+data /= 255.0
+data = np.expand_dims(data, -1)
+
+# Split data to train, val, test
+x_train = data[:3000]
+y_train = labels[:3000]
+
+x_val = data[3000:4000]
+y_val = labels[3000:4000]
+
+x_test = data[4000:]
+y_test = labels[4000:]
+
+print('Done.')
+
+# Save to data/processed
+print('Saving processed datasets...', end='')
+np.save(os.path.join(PROCESSED_DATA_PATH, 'x_train'), x_train)
+np.save(os.path.join(PROCESSED_DATA_PATH, 'x_val'), x_val)
+np.save(os.path.join(PROCESSED_DATA_PATH, 'x_test'), x_test)
+
+np.save(os.path.join(PROCESSED_DATA_PATH, 'y_train'), y_train)
+np.save(os.path.join(PROCESSED_DATA_PATH, 'y_val'), y_val)
+np.save(os.path.join(PROCESSED_DATA_PATH, 'y_test'), y_test)
+print('Done.')
